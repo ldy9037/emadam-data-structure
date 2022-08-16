c9ffd0bc34c25c29927bb04b2c85477e1baf5269
emadam <ldy9037@naver.com>
2022-08-16 20:08:06 +0900

    test: find 메서드에 대한 test case 작성

--- test/test_trie.py
+++ test/test_trie.py
@@ -31,6 +31,23 @@ class TrieTests(unittest.TestCase):
             
             if index >= 2: self.assertEqual(node.getLength(), [3])
             else: self.assertEqual(node.getLength(), [6,3])
+    
+    def test_save(self):
+        trie = Trie()
+
+        string = "emadam"
+        trie.save(string)
+
+        string = "eme"
+        trie.save(string)
+        
+        find_node = trie.find("emadam")
+        self.assertEqual(find_node.getLength(), [6])
+        self.assertEqual(find_node.getKey(), "m")
+        self.assertEqual(find_node.getNext(), {})
+
+        find_node = trie.find("emadw")
+        self.assertIsNone(find_node)
 
 if __name__ == '__main__':
     unittest.main()
